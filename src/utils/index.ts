641d6d0936e3a078dac98f629c940baa8454b8e3
陶启立 <taoqili@boulderaitech.com>
2022-10-13 17:37:39 +0800

    feat: 微前端改造

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,10 @@
+import { isQiankun } from "@/config";
+
 export * from './echarts'
 export * from './formatter'
 export { default as request } from './request'
 
 export const getAssets = (path: string): string => {
   const assets = '/assets/' + path.replace('/assets', '')
-  return process.env.PUBLIC_URL || ''  + assets.replace('//', '/')
+  return (isQiankun ? (window as any).__INJECTED_PUBLIC_PATH_BY_QIANKUN__ : process.env.PUBLIC_URL || '')  + assets.replace('//', '/')
 }
